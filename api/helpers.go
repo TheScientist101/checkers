e07fe63b2054e51b1153d4e9f65c752cf6ee0d5c
Urjith Mishra <66270347+TheScientist101@users.noreply.github.com>
2025-01-29 23:56:44 -0600

    Allow more headers for CORS
    Signed-off-by: Urjith Mishra <66270347+TheScientist101@users.noreply.github.com>

--- api/helpers.go
+++ api/helpers.go
@@ -5,6 +5,6 @@ import "net/http"
 func SetCors(w *http.ResponseWriter) {
 	(*w).Header().Set("Access-Control-Allow-Origin", "*")
 	(*w).Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
-	(*w).Header().Set("Access-Control-Allow-Headers", "Accept, Content-Type, Content-Length, Accept-Encoding")
+	(*w).Header().Set("Access-Control-Allow-Headers", "*")
 	(*w).WriteHeader(http.StatusOK)
 }
